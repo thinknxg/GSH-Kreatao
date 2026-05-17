fd6f566fdab5058837ec2d48df16cf448699658a
teraboxneed-design <teraboxneed@gmail.com>
2026-05-17 20:27:19 +0530

    fix: updated certificate JS files for spacing and content

--- gsh/gsh_kreatao/doctype/salary_transfer_certificate/salary_transfer_certificate.js
+++ gsh/gsh_kreatao/doctype/salary_transfer_certificate/salary_transfer_certificate.js
@@ -1,76 +1,33 @@
 // Copyright (c) 2025, Rawas and contributors
 // For license information, please see license.txt
-
-
 frappe.ui.form.on("Salary Transfer Certificate", {
-	refresh(frm) {
-
-	},
+    refresh(frm) {
+    },
     after_save: function(frm) {
-        // frm.save()
         if (!frm.is_new()) {
-            // Example dynamic values
             let re_number = frm.doc.name || "GMIS/2025/HR/144";
-            let certificate_date = frm.doc.posting_date || frappe.datetime.get_today(); // dynamic date
-            // Format date to DD/MM/YYYY
+            let certificate_date = frm.doc.posting_date || frappe.datetime.get_today();
             let parts = certificate_date.split("-");
             let formatted_date = parts[2] + "/" + parts[1] + "/" + parts[0];
-            // Dynamic bank details
-            let bank_name = frm.doc.bank_name || "____________";
-            let ac_no = frm.doc.ac_no || "____________";
-            // Set default for content_1 if empty
             if (!frm.doc.content_1) {
-                frm.set_value("content_1", `
-                    <p><b>Re Number: ${re_number}</b></p>
-                    <p><b>Date: ${formatted_date}</b></p>
-                    <br><br>
-                    <h4 style="text-align:center;"><u><b>SALARY TRANSFER CERTIFICATE</b></u></h4>
-                `);
+                frm.set_value("content_1", `<p style="margin:0;padding:0;"><b>Re Number: ${re_number}</b></p><p style="margin:0;padding:0;"><b>Date: ${formatted_date}</b></p><p style="text-align:center;margin:0;padding:0;"><u><b>SALARY TRANSFER CERTIFICATE</b></u></p>`);
             }
-    
-            // Set default for content_2 if empty
             if (!frm.doc.content_2) {
-                frm.set_value("content_2", `
-                    <div style="text-align: justify;">
-                        <p>This is to certify that the above-mentioned employee is working in Gulf Medical Integrated Services as per the information provided above. 
-                        Furthermore, as per the request we confirm that his/her monthly pay will be transferred to the above-mentioned bank account.</p>
-
-                        <p>This certificate has been issued at the request of the employee without any liability or commitment on the part of Gulf Medical 
-                        Integrated Services L.L.C toward any third party whatsoever.</p>
-
-                        <p><b>“The validity of the certificate shall be one month from the date of issue.”</b></p><br><br>
-
-                        <p><b>Your Sincerely,</b></p><br><br>
-
-                        <p><b>Zuhair Al Abduwani<br>CEO</b></p>
-                    </div>
-                `);
+                frm.set_value("content_2", `<div style="text-align: justify;"><p>This is to certify that the above-mentioned employee is working in Gulf Medical Integrated Services L.L.C as per the information provided above. Furthermore, as per the request we confirm that his/her monthly pay will be transferred to the above-mentioned bank account.</p><p>This certificate has been issued at the request of the employee without any liability or commitment on the part of Gulf Medical Integrated Services L.L.C toward any third party whatsoever.</p><p><b>"The validity of the certificate shall be one month from the date of issue."</b></p><br><br><p><b>Your Sincerely,</b></p><br><br><p><b>Zuhair Al Abduwani<br>CEO</b></p></div>`);
             }
         }
         frm.save()
     },
     employee: function(frm) {
-        console.log("Employee selected:", frm.doc.employee);
-        
         frappe.call({
             method: 'gsh.gsh_kreatao.doctype.salary_transfer_certificate.salary_transfer_certificate.get_salary_detail',
             args: {
                 employee: frm.doc.employee
             },
             callback: function(r) {
-                console.log("Response:", r);
-                
                 if (r.message) {
-                    console.log("Message received:", r.message);
-                    
-                    
                     frm.set_value("salary", r.message.base);
-                } else {
-                    console.log("No message received.");
                 }
-            },
-            error: function(err) {
-                console.log("Error occurred:", err);
             }
         });
     }
